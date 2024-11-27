6762fbb0101e61b83e47271d03b17b272e41d8f6
LeandroMQ2018 <deinermamani34@gmail.com>
2024-11-26 23:24:07 -0400

    Update DashboardProfesorPage.jsx

--- src/pages/DashboardProfesorPage.jsx
+++ src/pages/DashboardProfesorPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { API_URL } from '../config';
 function DashboardProfesorPage() {
   const [tareas, setTareas] = useState([]);
   const [estudiantes, setEstudiantes] = useState([]);
